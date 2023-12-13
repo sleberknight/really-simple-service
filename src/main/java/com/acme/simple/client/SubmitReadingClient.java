c477f64e62e3ee03176e8f3ad004ee058ddae289
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-12-13 21:14:09 +0000

    Allow SubmitReadingClient to submit or validate
    * Use argparse4j to parse arguments
    * Allow submitting or validating a reading. Validation uses server's configuration!

--- src/main/java/com/acme/simple/client/SubmitReadingClient.java
+++ src/main/java/com/acme/simple/client/SubmitReadingClient.java
@@ -7,6 +7,10 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.dropwizard.jersey.jackson.JacksonFeature;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 import lombok.extern.slf4j.Slf4j;
 import org.glassfish.jersey.CommonProperties;
 import org.yaml.snakeyaml.Yaml;
@@ -22,11 +26,15 @@ import java.util.random.RandomGenerator;
 public class SubmitReadingClient {
 
     public static void main(String[] args) throws IOException {
-        // Get the configured value for timestamp serialization
-        var serializeTimestampsAsMillis = readTimestampSerializationConfig();
-        LOG.info("serializeTimestampsAsMillis: {}", serializeTimestampsAsMillis);
+        var config = parseArgs(args);
+        var action = config.action();
+        var serializeTimestampsAsMillis = config.serializeTimestampsAsMillis();
 
-        // Create object mapper, register Java 8 date/time support, and configure timestamp serialization
+        LOG.info("Using action: {}", action);
+        LOG.info("Using serializeTimestampsAsMillis: {}", serializeTimestampsAsMillis);
+
+        // Create object mapper, register Java 8 date/time support, and configure
+        // timestamp serialization
         var mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         JacksonHelpers.configureObjectMapper(mapper, serializeTimestampsAsMillis);
@@ -43,7 +51,8 @@ public class SubmitReadingClient {
             var randomTemp = RandomGenerator.getDefault().nextDouble(27.0, 50.0);
             var reading = new TemperatureReading(randomTemp, Instant.now());
 
-            try (var response = client.target("http://localhost:8080/temps/new")
+            try (var response = client.target("http://localhost:8080/temps")
+                    .path(action)
                     .request("application/json")
                     .post(Entity.json(reading))) {
 
@@ -55,6 +64,53 @@ public class SubmitReadingClient {
         } finally {
             client.close();
         }
+    }
+
+    private record Config(String action, boolean serializeTimestampsAsMillis) {
+    }
+
+    private static Config parseArgs(String[] args) throws IOException {
+        var parser = ArgumentParsers.newFor("SubmitReadingClient")
+                .build()
+                .defaultHelp(true)
+                .description("Submit or validate a temperature reading");
+
+        parser.addArgument("-a", "--action")
+                .required(false)
+                .choices("submit", "validate")
+                .setDefault("submit")
+                .help("Submit or validate a reading. Note that validation is server-side using the server's timestamp serialization configuration.");
+
+        parser.addArgument("-s", "--serialization")
+                .required(false)
+                .choices("millis", "nanos", "from-config-file")
+                .setDefault("from-config-file")
+                .help("How to configure timestamp serialization (applies only to client)");
+
+        String action = null;
+        boolean serializeTimestampsAsMillis = false;
+        try {
+            Namespace ns = parser.parseArgs(args);
+            var actionOpt = ns.getString("action");
+            action = switch (actionOpt) {
+                case "submit" -> "new";
+                case "validate" -> "validateReading";
+                default -> "new";
+            };
+
+            var serializationOpt = ns.getString("serialization");
+            serializeTimestampsAsMillis = switch (serializationOpt) {
+                case "millis" -> true;
+                case "nanos" -> false;
+                case "from-config-file" -> readTimestampSerializationConfig();
+                default -> true;
+            };
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
+            System.exit(1);
+        }
+
+        return new Config(action, serializeTimestampsAsMillis);
     }
 
     @SuppressWarnings("unchecked")
